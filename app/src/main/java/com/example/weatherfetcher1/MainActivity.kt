2683f31b2943200b03262b813b3268fde0b5da2a
flickktv <tkachev.03@yandex.ru>
2022-11-05 15:10:26 +0500

    screen

--- app/src/main/java/com/example/weatherfetcher1/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.weatherfetcher1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.textView2)
+        textViewHello.text = "Hello from code"
+
+        val btnWeatherScreen = findViewById<Button>(R.id.btnWeather)
+        btnWeatherScreen.setOnClickListener {
+            Intent(this, WeatherScreen::class.java).also { startActivity(it) }
+        }
     }
 }
